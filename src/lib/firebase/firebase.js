1b36cced97496d74af7676701fd4f6e31c8a3fd8
Abdullah Adeel <easyautosol@gmail.com>
2021-07-17 01:18:24 +0500

    removed firebase config add to .gitignore

--- src/lib/firebase/firebase.js
+++ src/lib/firebase/firebase.js
@@ -1,19 +1,8 @@
-import Firebase from 'firebase/app';
-import 'firebase/firestore';
-import auth from 'firebase';
-
-
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-    apiKey: "AIzaSyDaNnTKHcJ6sioyEg1dDdgDRTvsNn03fJQ",
-    authDomain: "netflix-pro-clone.firebaseapp.com",
-    databaseURL: "https://netflix-pro-clone.firebaseio.com",
-    projectId: "netflix-pro-clone",
-    storageBucket: "netflix-pro-clone.appspot.com",
-    messagingSenderId: "785411405957",
-    appId: "1:785411405957:web:009b18701bb0afb81651ec",
-    measurementId: "G-65ZFEKSTPY"
-};
+import Firebase from "firebase/app";
+import "firebase/firestore";
+import { firebaseConfig } from "./firebaseConfig";
+// firebase configs are not published to git. You can create on your own.
+// After that create a file name "firebaseConfig.js" in the same dir and paste the your config there
 
 const firebase = Firebase.initializeApp(firebaseConfig);
 
