8499a41f9c6ff07b2d41207426d0912c59fab7c5
Abdullah Adeel <easyautosol@gmail.com>
2021-07-17 02:51:56 +0500

    changed config

---
+++ src/lib/firebase/firebaseConfig.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDaNnTKHcJ6sioyEg1dDdgDRTvsNn03fJQ",
+  authDomain: "netflix-pro-clone.firebaseapp.com",
+  databaseURL: "https://netflix-pro-clone.firebaseio.com",
+  projectId: "netflix-pro-clone",
+  storageBucket: "netflix-pro-clone.appspot.com",
+  messagingSenderId: "785411405957",
+  appId: "1:785411405957:web:009b18701bb0afb81651ec",
+  measurementId: "G-65ZFEKSTPY",
+};
