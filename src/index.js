9cc9ccf8a058c525513ef9a3c3b0870616f67022
Abdullah Adeel <easyautosol@gmail.com>
2021-07-17 03:34:34 +0500

    update firebase setup ✌✌✅

--- src/index.js
+++ src/index.js
@@ -1,28 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
 // Global Styles
-import { GlobalStyles } from './globalstyles';
+import { GlobalStyles } from "./globalstyles";
 // normalize.css
-import 'normalize.css';
+import "normalize.css";
 
+// Firebase App (the core Firebase SDK) is always required and must be listed first
+// eslint-disable-next-line no-unused-vars
+import firebase from "firebase/app";
+// If you are using v7 or any earlier version of the JS SDK, you should import firebase using namespace import
+// import * as firebase from "firebase/app"
+
+// If you enabled Analytics in your project, add the Firebase SDK for Analytics
+import "firebase/analytics";
+
+// Add the Firebase products that you want to use
+import "firebase/auth";
+import "firebase/firestore";
 
 // Redux
-import store from './Redux/app/store';
-import { Provider } from 'react-redux';
+import store from "./Redux/app/store";
+import { Provider } from "react-redux";
 
 ReactDOM.render(
-    <>
-        <Provider store={store}>
-            <GlobalStyles />
-            <App />
-        </Provider>
-    </>
-    , document.getElementById('root')
+  <>
+    <Provider store={store}>
+      <GlobalStyles />
+      <App />
+    </Provider>
+  </>,
+  document.getElementById("root")
 );
-
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
