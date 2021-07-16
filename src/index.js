6b5ecf572fa9ba7265a779a80038699782cbfa2c
Abdullah Adeel <easyautosol@gmail.com>
2021-07-17 03:41:51 +0500

    update actions

--- src/index.js
+++ src/index.js
@@ -7,19 +7,6 @@ import { GlobalStyles } from "./globalstyles";
 // normalize.css
 import "normalize.css";
 
-// Firebase App (the core Firebase SDK) is always required and must be listed first
-// eslint-disable-next-line no-unused-vars
-import firebase from "firebase/app";
-// If you are using v7 or any earlier version of the JS SDK, you should import firebase using namespace import
-// import * as firebase from "firebase/app"
-
-// If you enabled Analytics in your project, add the Firebase SDK for Analytics
-import "firebase/analytics";
-
-// Add the Firebase products that you want to use
-import "firebase/auth";
-import "firebase/firestore";
-
 // Redux
 import store from "./Redux/app/store";
 import { Provider } from "react-redux";
