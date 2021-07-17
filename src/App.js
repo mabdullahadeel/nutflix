96f6d112fb66bc8859d6e5d27bc43fb638b92085
Abdullah Adeel <64294045+mabdullahadeel@users.noreply.github.com>
2021-07-18 01:09:56 +0500

    update App.js

--- src/App.js
+++ src/App.js
@@ -13,19 +13,6 @@ import { logIn, selectUser } from "./Redux/features/userSlice";
 import { firebase } from "./lib/firebase/firebase";
 import { getUserDataFromLS } from "./Redux/features/utility";
 
-// Firebase extras
-// Firebase App (the core Firebase SDK) is always required and must be listed first
-import firebase from "firebase/app";
-// If you are using v7 or any earlier version of the JS SDK, you should import firebase using namespace import
-// import * as firebase from "firebase/app"
-
-// If you enabled Analytics in your project, add the Firebase SDK for Analytics
-import "firebase/analytics";
-
-// Add the Firebase products that you want to use
-import "firebase/auth";
-import "firebase/firestore";
-
 
 function App() {
   const user = useSelector(selectUser);
