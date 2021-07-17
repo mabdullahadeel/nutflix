ae96d150c075328faf2068cd83540989e371f277
Abdullah Adeel <64294045+mabdullahadeel@users.noreply.github.com>
2021-07-18 01:10:43 +0500

    add default firebase cnfigs

--- src/index.js
+++ src/index.js
@@ -11,6 +11,20 @@ import "normalize.css";
 import store from "./Redux/app/store";
 import { Provider } from "react-redux";
 
+
+// Firebase extras
+// Firebase App (the core Firebase SDK) is always required and must be listed first
+import firebase from "firebase/app";
+// If you are using v7 or any earlier version of the JS SDK, you should import firebase using namespace import
+// import * as firebase from "firebase/app"
+
+// If you enabled Analytics in your project, add the Firebase SDK for Analytics
+import "firebase/analytics";
+
+// Add the Firebase products that you want to use
+import "firebase/auth";
+import "firebase/firestore";
+
 ReactDOM.render(
   <>
     <Provider store={store}>
